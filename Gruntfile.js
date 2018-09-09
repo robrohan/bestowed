1d971b619c45197cc45a774bd75557bb45967c05
Rob Rohan <robrohan@gmail.com>
2018-09-09 15:53:05 +1200

    Adding a linter and making it mostly happy

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,75 +1,75 @@
 module.exports = function(grunt) {
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
 
-        jshint: {
-            files: [
-                'Gruntfile.js',
-                'src/**/*.js',
-                'test/**/*.js'
-            ],
-            options: {
-                globals: {
-                    jQuery: false
-                }
-            }
+    jshint: {
+      files: [
+        'Gruntfile.js',
+        'src/**/*.js',
+        'test/**/*.js',
+      ],
+      options: {
+        globals: {
+          jQuery: false,
         },
+      },
+    },
 
-        mocha: {
-            all: {
-                src: ['tests/testrunner.html'],
-            },
-            options: {
-                run: true
-            }
-        },
+    mocha: {
+      all: {
+        src: ['tests/testrunner.html'],
+      },
+      options: {
+        run: true,
+      },
+    },
 
-        uglify: {
-            options: {
-                banner: '/*! <%= pkg.name %> <%= grunt.template.today(\"yyyy-mm-dd\") %> */\n'
-            },
-            build: {
-                src: 'src/<%= pkg.name %>.js',
-                dest: 'build/<%= pkg.name %>.min.js'
-            }
-        },
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today(\"yyyy-mm-dd\") %> */\n',
+      },
+      build: {
+        src: 'src/<%= pkg.name %>.js',
+        dest: 'build/<%= pkg.name %>.min.js',
+      },
+    },
 
-        cssmin: {
-            target: {
-                files: [{
-                    src: 'src/<%= pkg.name %>.css',
-                    dest: 'build/<%= pkg.name %>.min.css',
-                }]
-            }
-        },
+    cssmin: {
+      target: {
+        files: [{
+          src: 'src/<%= pkg.name %>.css',
+          dest: 'build/<%= pkg.name %>.min.css',
+        }],
+      },
+    },
 
-        copy: {
-            main: {
-                files: [
-                    {expand: true, cwd: 'src/', src: ['themes/**'], dest: 'build/'},
-                ],
-            },
-        },
+    copy: {
+      main: {
+        files: [
+          {expand: true, cwd: 'src/', src: ['themes/**'], dest: 'build/'},
+        ],
+      },
+    },
 
-        clean: {
-            build: {
-                src: ["build"]
-            }
-        },
+    clean: {
+      build: {
+        src: ['build'],
+      },
+    },
 
-        watch: {
-            files: ['<%= jshint.files %>'],
-            tasks: ['jshint']
-        }
-    });
+    watch: {
+      files: ['<%= jshint.files %>'],
+      tasks: ['jshint'],
+    },
+  });
 
-    grunt.loadNpmTasks('grunt-mocha');
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-cssmin');
-    grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-mocha');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-clean');
 
-    grunt.registerTask('default', ['clean', 'jshint', 'uglify', 'cssmin', 'copy']);
-    grunt.registerTask('watch', ['jshint', 'uglify', 'watch']);
+  grunt.registerTask('default', ['clean', 'jshint', 'uglify', 'cssmin', 'copy']);
+  grunt.registerTask('watch', ['jshint', 'uglify', 'watch']);
 };
--- src/bestowed.js
+++ src/bestowed.js
@@ -1,183 +1,183 @@
-var bestowed = bestowed || {};
+// Comment
 var exports = exports || {};
+var bestowed = bestowed || {};
 
-(function(){
-	"use strict";
+(function() {
+  'use strict';
 
-	bestowed.current = 0;
-	bestowed.maxSlides = 0;
-	bestowed.slideCache = null;
+  bestowed.current = 0;
+  bestowed.maxSlides = 0;
+  bestowed.slideCache = null;
 
-	bestowed.cachedHead = null;
-	bestowed.CSS_ENTRY = '/css/main.css';
-	bestowed.JS_ENTRY = '/js/main.js';
-	bestowed.CSS_FILE = 'bestowed.min.css';
-	bestowed.fullPath = null;
+  bestowed.cachedHead = null;
+  bestowed.CSS_ENTRY = '/css/main.css';
+  bestowed.JS_ENTRY = '/js/main.js';
+  bestowed.CSS_FILE = 'bestowed.min.css';
+  bestowed.fullPath = null;
 
-	bestowed.init = function() {
-		// cache the slides
-		bestowed.slideCache = bestowed.slides();
-		// and the length
-		bestowed.maxSlides = bestowed.slideCache.length;
-		// and the path to this script
-		bestowed.fullPath = bestowed.scriptPath(document.getElementsByTagName("script"));
+  bestowed.init = function() {
+    // cache the slides
+    bestowed.slideCache = bestowed.slides();
+    // and the length
+    bestowed.maxSlides = bestowed.slideCache.length;
+    // and the path to this script
+    bestowed.fullPath = bestowed.scriptPath(document.getElementsByTagName('script'));
 
-		bestowed.changeSlide(0);
-		bestowed.current = 0;
+    bestowed.changeSlide(0);
+    bestowed.current = 0;
 
-		// Add some meta headers that org-mode export doesn't do
-		bestowed.writeMeta({'charset':'utf-8'});
-		bestowed.writeMeta({'http-equiv':'X-UA-Compatible', 'content':"IE=edge,chrome=1"});
-		bestowed.writeMeta({'name':"viewport", 'content':"width=device-width, initial-scale=1"});
+    // Add some meta headers that org-mode export doesn't do
+    bestowed.writeMeta({
+      'charset': 'utf-8',
+    });
+    bestowed.writeMeta({
+      'http-equiv': 'X-UA-Compatible',
+      'content': 'IE=edge,chrome=1',
+    });
+    bestowed.writeMeta({
+      'name': 'viewport',
+      'content': 'width=device-width, initial-scale=1',
+    });
 
-		// Load the basic system css file
-		bestowed.writeStyle(bestowed.fullPath + bestowed.CSS_FILE);
+    // Load the basic system css file
+    bestowed.writeStyle(bestowed.fullPath + bestowed.CSS_FILE);
 
-		// Try to load the theme specified in the org file
-		var theme = bestowed.findThemePath();
-		if(bestowed.isLocalPath(theme)) {
-			theme = bestowed.fullPath + theme;
-		}
-		try {
-			bestowed.writeStyle(theme + bestowed.CSS_ENTRY);
-			bestowed.writeScript(theme + bestowed.JS_ENTRY);
-		} catch(e) {
-			console.log("Failed to load theme. " + e);
-		}
-	};
+    // Try to load the theme specified in the org file
+    var theme = bestowed.findThemePath();
+    if (bestowed.isLocalPath(theme)) {
+      theme = bestowed.fullPath + theme;
+    }
+    try {
+      bestowed.writeStyle(theme + bestowed.CSS_ENTRY);
+      bestowed.writeScript(theme + bestowed.JS_ENTRY);
+    } catch (e) {
+      console.log('Failed to load theme. ' + e);
+    }
+  };
 
-	bestowed.isLocalPath = function(path) {
-		if(path.toString().match(/^http/) || path.toString().match(/^file/)) {
-			return false;
-		}
-		return true;
-	};
+  bestowed.isLocalPath = function(path) {
+    if (path.toString().match(/^http/) || path.toString().match(/^file/)) {
+      return false;
+    }
+    return true;
+  };
 
-	bestowed.scriptPath = function(scriptsArray) {
-		if(bestowed.fullPath) {
-			return bestowed.fullPath;
-		}
-		var givenPath = scriptsArray[scriptsArray.length-1].src;
-		var parts = givenPath.split('/');
-		parts.pop();
-		var newPath = parts.join("/");
-		return newPath + '/';
-	};
+  bestowed.scriptPath = function(scriptsArray) {
+    if (bestowed.fullPath) {
+      return bestowed.fullPath;
+    }
+    var givenPath = scriptsArray[scriptsArray.length - 1].src;
+    var parts = givenPath.split('/');
+    parts.pop();
+    var newPath = parts.join('/');
+    return newPath + '/';
+  };
 
-	bestowed.writeStyle = function(path) {
-		bestowed.cachedHead = bestowed.cachedHead || document.getElementsByTagName("head")[0];
-		var link = document.createElement('link');
-		link.setAttribute("rel", "stylesheet");
-		link.setAttribute("type", "text/css");
-		link.setAttribute("href", path);
-		bestowed.cachedHead.appendChild(link);
-	};
+  bestowed.writeStyle = function(path) {
+    bestowed.cachedHead = bestowed.cachedHead || document.getElementsByTagName('head')[0];
+    var link = document.createElement('link');
+    link.setAttribute('rel', 'stylesheet');
+    link.setAttribute('type', 'text/css');
+    link.setAttribute('href', path);
+    bestowed.cachedHead.appendChild(link);
+  };
 
-	bestowed.writeScript = function(path) {
-		bestowed.cachedHead = bestowed.cachedHead || document.getElementsByTagName("head")[0];
-		var script = document.createElement('script');
-		script.setAttribute("type", "text/javascript");
-		script.setAttribute("src", path);
-		bestowed.cachedHead.appendChild(script);
-	};
+  bestowed.writeScript = function(path) {
+    bestowed.cachedHead = bestowed.cachedHead || document.getElementsByTagName('head')[0];
+    var script = document.createElement('script');
+    script.setAttribute('type', 'text/javascript');
+    script.setAttribute('src', path);
+    bestowed.cachedHead.appendChild(script);
+  };
 
-	bestowed.writeMeta = function(settings) {
-		bestowed.cachedHead = bestowed.cachedHead || document.getElementsByTagName("head")[0];
-		var meta = document.createElement('meta');
+  bestowed.writeMeta = function(settings) {
+    bestowed.cachedHead = bestowed.cachedHead || document.getElementsByTagName('head')[0];
+    var meta = document.createElement('meta');
 
-		for(var k in settings) {
-			meta.setAttribute(k, settings[k]);
-		}
+    for (var k in settings) {
+      if (settings.hasOwnProperty(k)) {
+        meta.setAttribute(k, settings[k]);
+      }
+    }
 
-		bestowed.cachedHead.appendChild(meta);
-	};
+    bestowed.cachedHead.appendChild(meta);
+  };
 
-	bestowed.findThemePath = function() {
-		var metas = document.getElementsByTagName("meta");
-		var mlen = metas.length;
-		for(var x=0; x<mlen; x++) {
-			var metaName = metas[x].getAttribute('name');
-			if(metaName === 'bestowed-theme') {
-				return metas[x].getAttribute('value');
-			}
-		}
-	};
+  bestowed.findThemePath = function() {
+    var metas = document.getElementsByTagName('meta');
+    var mlen = metas.length;
+    for (var x = 0; x < mlen; x++) {
+      var metaName = metas[x].getAttribute('name');
+      if (metaName === 'bestowed-theme') {
+        return metas[x].getAttribute('value');
+      }
+    }
+  };
 
-	bestowed.slides = function() {
-		if(bestowed.slideCache) {
-			return bestowed.slideCache;
-		}
-		return document.getElementsByClassName('outline-2');
-	};
+  bestowed.slides = function() {
+    if (bestowed.slideCache) {
+      return bestowed.slideCache;
+    }
+    return document.getElementsByClassName('outline-2');
+  };
 
-	bestowed.changeSlide = function(to) {
-		if(to >= bestowed.maxSlides || to < 0) {
-			return;
-		}
-		var current = document.getElementsByClassName('current');
-		var previous = document.getElementsByClassName('previous');
+  bestowed.changeSlide = function(to) {
+    if (to >= bestowed.maxSlides || to < 0) {
+      return;
+    }
+    var current = document.getElementsByClassName('current');
+    var previous = document.getElementsByClassName('previous');
 
-		if(previous[0]) {
-			bestowed.removeClass(previous[0], 'previous');
-		}
+    if (previous[0]) {
+      bestowed.removeClass(previous[0], 'previous');
+    }
 
-		if(current[0]) {
-			bestowed.replaceClass(current[0], 'current', 'previous');
-		}
+    if (current[0]) {
+      bestowed.replaceClass(current[0], 'current', 'previous');
+    }
 
-		bestowed.slides()[to].className += ' current';
-		bestowed.current = to;
-	};
+    bestowed.slides()[to].className += ' current';
+    bestowed.current = to;
+  };
 
-	bestowed.nextSlide = function() {
-		bestowed.changeSlide(bestowed.current + 1);
-	};
+  bestowed.nextSlide = function() {
+    bestowed.changeSlide(bestowed.current + 1);
+  };
 
-	bestowed.previousSlide = function() {
-		bestowed.changeSlide(bestowed.current - 1);
-	};
+  bestowed.previousSlide = function() {
+    bestowed.changeSlide(bestowed.current - 1);
+  };
 
-	bestowed.removeClass = function(element, theClass) {
-		var re = new RegExp(theClass,"g");
-		element.className = element.className.replace(re, '');
-	};
+  bestowed.removeClass = function(element, theClass) {
+    var re = new RegExp(theClass, 'g');
+    element.className = element.className.replace(re, '');
+  };
 
-	bestowed.replaceClass = function(element, origClass, newClass) {
-		var re = new RegExp(origClass,"g");
-		element.className = element.className.replace(re, newClass);
-	};
+  bestowed.replaceClass = function(element, origClass, newClass) {
+    var re = new RegExp(origClass, 'g');
+    element.className = element.className.replace(re, newClass);
+  };
 
-	//////////////////////////////////////////////////////////////////////////
-	/* bestowed.handleClick = function(e) {
-		if(e.altKey === true) {
-			bestowed.previousSlide();
-		} else {
-			bestowed.nextSlide();
-		}
-		e.preventDefault();
-		return false;
-	}; */
+  bestowed.handleKeyDown = function(e) {
+    if (e.keyCode === 40 || e.keyCode === 39 | e.keyCode === 32 | e.keyCode === 74) {
+      bestowed.nextSlide();
+      e.preventDefault();
+      return false;
+    } else if (e.keyCode === 38 || e.keyCode === 37 || e.keyCode === 8 || e.keyCode === 75) {
+      bestowed.previousSlide();
+      e.preventDefault();
+      return false;
+    }
+  };
 
-	bestowed.handleKeyDown = function(e) {
-		if(e.keyCode === 40 || e.keyCode === 39 | e.keyCode === 32 | e.keyCode === 74) {
-			bestowed.nextSlide();
-			e.preventDefault();
-			return false;
-		} else if (e.keyCode === 38 || e.keyCode === 37 || e.keyCode === 8 || e.keyCode === 75) {
-			bestowed.previousSlide();
-			e.preventDefault();
-			return false;
-		}
-	};
-
-	bestowed.register = function() {
-		if(typeof(window) !== 'undefined') {
-			// Register our listeners
-			window.addEventListener("load", bestowed.init);
-			// document.addEventListener("click", bestowed.handleClick);
-			window.addEventListener("keydown", bestowed.handleKeyDown);
-		}
-	};
+  bestowed.register = function() {
+    if (typeof (window) !== 'undefined') {
+      // Register our listeners
+      window.addEventListener('load', bestowed.init);
+      // document.addEventListener('click', bestowed.handleClick);
+      window.addEventListener('keydown', bestowed.handleKeyDown);
+    }
+  };
 })();
 
 bestowed.register();
